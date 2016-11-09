1c1c0aa1f6cbc9c1389762fdefd4306c5b4af4eb
Paras Dahal <shree5paras@gmail.com>
2016-11-09 11:10:19 +0530

    Implemented backpropagation. Testing it with various network architecture.

--- tinyml/FullyConnectedNN.py
+++ tinyml/FullyConnectedNN.py
@@ -50,45 +50,51 @@ class FullyConnectedNN:
 
 		for weight,bias in zip(self.weights,self.biases):
 
-			z = activation.dot(weight) + bias
+			z = activation.dot(weight)
 			activation = afunc(z)
 			zs.append(z)
 			activations.append(activation)
 
 		return (zs,activations)
 
-	def train(self,alpha):
+	def train(self,X,y,alpha,reg):
 		
 		weights = list() # list of weights per each layer
 		biases = list() #list of biases for each layer
 
-		for i in range(1,len(shape)):
+		for i in range(1,len(self.shape)):
 			# size of weight matrix for each layer => (num of units in this layer * num of units in next layer)
-			self.weights.append(np.random.random_sample((shape[i-1],shape[i])))
-			self.biases.append(np.zeros((1,shape[i])))
+			weights.append(np.random.random_sample((self.shape[i-1],self.shape[i])))
+			biases.append(np.zeros((1,self.shape[i])))
 		
 		temp_weight = weights
 		temp_bias = biases
 		# iterate over all rows in the dataset
-		for x,y in zip(self.X,self.y):
+		for x,y in zip(X,y):
 
 			zs,activations = self.predict(x)
 			#last layer
 			delta = activations[-1] - y
-			temp_weight[-1] = np.dot(delta,activations[-1])
+			temp_weight[-1] = np.dot(delta,activations[-1].T)
 			temp_bias[-1] = delta.sum()
 			# peroform backward pass for each layer except last
 			for l in range(1,len(self.shape)):
 				delta = np.dot(delta,self.weights[-l]) * (activations[-l]*(1-activations[-l]))
-				temp_weight[-l] = delta.activations[-l]
+				temp_weight[-l] = delta.dot(activations[-l].T)
 				temp_bias[-l] = delta.sum()
 
-			for weight,d in zip(weights,temp_weight):
-				weight += -alpha*d
-			
-			for bias,d in z(weights,temp_bias):
+			# gradient descent parameter update for weights
+			for weight,dWeight in zip(weights,temp_weight):
+				# apply regularization to the weight
+				dWeight += reg * weight
+				weight += -alpha*dWeight
+
+			# gradient descent parameter update for biases
+			for bias,d in zip(weights,temp_bias):
 				bias += -alpha*d
 		
+		self.weights = weights
+		self.biases = biases
 		return (weights,biases)
 
 
@@ -111,12 +117,31 @@ class FullyConnectedNN:
 		from matplotlib import pyplot as plt
 		plt.scatter(self.X[:,0],self.X[:,1],s=40, c=y, cmap=plt.cm.Spectral)
 		plt.show()
+	
+	# def plot_decision_boundary(self,X,y):
+	# 	# Set min and max values and give it some padding
+	# 	x_min, x_max = X[:, 0].min() - .5, X[:, 0].max() + .5
+	# 	y_min, y_max = X[:, 1].min() - .5, X[:, 1].max() + .5
+	# 	h = 0.01
+	# 	# Generate a grid of points with distance h between them
+	# 	xx, yy = np.meshgrid(np.arange(x_min, x_max, h), np.arange(y_min, y_max, h))
+	# 	# Predict the function value for the whole gid
+	# 	Z = self.predict(np.c_[xx.ravel(), yy.ravel()])[1][-1]
+	# 	print(Z.shape)
+	# 	Z = Z.reshape(xx.shape)
+	# 	# Plot the contour and training examples
+	# 	plt.contourf(xx, yy, Z, cmap=plt.cm.Spectral)
+	# 	plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Spectral)
+	# 	plt.show()
+
 
 if __name__ == "__main__":
 	from sklearn import datasets
 	X,y = datasets.make_moons(200,noise=0.2)
 	print(np.shape(X))
-	NN = FullyConnectedNN([2,2,2,2],X,y)
+	print((X[1],y[1]))
+	NN = FullyConnectedNN([2,2,1],X,y)
 	NN.plot_data()
-	print(NN.predict(X[1]))
-	# NN.back_propagation()
+	print(NN.train(X,y,alpha=0.3,reg=0.1))
+	NN.predict(X[1])
+	# NN.plot_decision_boundary(X,y)
