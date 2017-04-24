de1ab3854571b303f49b320b369f2cd404114a2d
Paras Dahal <shree5paras@gmail.com>
2017-04-24 10:42:18 +0530

    refactored examples.py

--- examples.py
+++ examples.py
@@ -4,15 +4,15 @@ from tinyml import LogisticRegression as lo
 from sklearn import datasets
 
 # Linear Regression
-# X,y = datasets.make_regression(n_features=1,n_informative=1, noise=20, random_state=1)
-# table=np.column_stack((X,y))
+X,y = datasets.make_regression(n_features=1,n_informative=1, noise=20, random_state=1)
+table=np.column_stack((X,y))
 
-# p = lr.LinearRegression(table,reg=True,lamda=10)
-# p.gradient_descent(1000,0.01)
-# print(p.accuracy())
-# p.plot_fit()
+p = lr.LinearRegression(table,reg=True,lamda=10)
+p.gradient_descent(1000,0.01)
+print(p.accuracy())
+p.plot_fit()
 
-# Logistic Regression Classification
+# Logistic Regression 
 
 x,y = datasets.make_classification(n_features=2, n_redundant=0, n_informative=2,n_clusters_per_class=1)
 table=np.column_stack((x,y))
